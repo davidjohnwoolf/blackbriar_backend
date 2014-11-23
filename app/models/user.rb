e7e2df825690fdea151154dae10a794880a98873
David Woolf <davidjohnwoolf@gmail.com>
2014-11-22 21:37:17 -0700

    update user model to handle password hashing

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,18 @@
 class User < ActiveRecord::Base
+  # runs the encrypt_password method before a user is saved
+  before_save :encrypt_password
+
+  # stores a temporary password taken from the new user form
+  attr_accessor :password
+
+  # another virtual attribute for password confirmation
+  validates_confirmation_of :password
+
+  def encrypt_password
+    # generate password_salt
+    self.password_salt = BCrypt::Engine.generate_salt
+    
+    # generate password_hash from password + password_salt
+    self.password_hash = BCrypt::Engine.hash_secret(password, password_salt)
+  end
 end
