cf3c8ec574c678697c02e344943d6a31bb952f60
David Woolf <davidjohnwoolf@gmail.com>
2014-11-28 23:42:46 -0700

    generate admin session controller

---
+++ app/controllers/admin_sessions_controller.rb
@@ -0,0 +1,22 @@
+class AdminSessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    @admin = Admin.authenticate(params[:email], params[:password])
+    if @admin
+      flash[:notice] = 'Successfully logged In as Admin'
+      session[:admin_id] = @admin.id
+      redirect_to '/'
+    else
+      flash[:alert] = 'Error logging in as Admin'
+      redirect_to log_in_path
+    end
+  end
+
+  def destroy
+    session[:admin_id] = nil
+    flash[:notice] = 'Successfully logged out'
+    redirect_to '/'
+  end
+end
