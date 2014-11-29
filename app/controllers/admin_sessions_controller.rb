971431ebb4712ccf28fc83a10dabf70370e81948
David Woolf <davidjohnwoolf@gmail.com>
2014-11-29 03:07:39 -0700

    update user and admin sessions controllers to throw notice when trying to login while already logged in

--- app/controllers/admin_sessions_controller.rb
+++ app/controllers/admin_sessions_controller.rb
@@ -3,20 +3,25 @@ class AdminSessionsController < ApplicationController
   end
 
   def create
-    @admin = Admin.authenticate(params[:email], params[:password])
-    if @admin
-      flash[:notice] = 'Successfully logged In as Admin'
-      session[:admin_id] = @admin.id
-      redirect_to '/'
+    if !session[:user_id] && !session[:admin_id]
+      @admin = Admin.authenticate(params[:email], params[:password])
+      if @admin
+        flash[:notice] = 'Successfully logged In as Admin'
+        session[:admin_id] = @admin.id
+        redirect_to root_url
+      else
+        flash[:alert] = 'Error logging in as Admin'
+        redirect_to log_in_admin_url
+      end
     else
-      flash[:alert] = 'Error logging in as Admin'
-      redirect_to log_in_admin_path
+      flash[:alert] = 'Already Logged In'
+      redirect_to root_url
     end
   end
 
   def destroy
     session[:admin_id] = nil
     flash[:notice] = 'Successfully logged out'
-    redirect_to '/'
+    redirect_to root_url
   end
 end
--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -3,20 +3,25 @@ class UserSessionsController < ApplicationController
   end
 
   def create
-    @user = User.authenticate(params[:email], params[:password])
-    if @user
-      flash[:notice] = 'Successfully logged In'
-      session[:user_id] = @user.id
-      redirect_to '/'
+    if !session[:user_id] && !session[:admin_id]
+      @user = User.authenticate(params[:email], params[:password])
+      if @user
+        flash[:notice] = 'Successfully logged In'
+        session[:user_id] = @user.id
+        redirect_to root_url
+      else
+        flash[:alert] = 'Error logging in'
+        redirect_to log_in_url
+      end
     else
-      flash[:alert] = 'Error logging in'
-      redirect_to log_in_path
+      flash[:alert] = 'Already Logged In'
+      redirect_to root_url
     end
   end
 
   def destroy
     session[:user_id] = nil
     flash[:notice] = 'Successfully logged out'
-    redirect_to '/'
+    redirect_to root_url
   end
 end
