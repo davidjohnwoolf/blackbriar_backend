f3b9b42b2373e0199fc84c5f5cf22a43017f3f0f
David Woolf <davidjohnwoolf@gmail.com>
2014-11-28 23:44:28 -0700

    add current admin method to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,5 +7,10 @@ class ApplicationController < ActionController::Base
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 
+  def current_admin
+    @current_admin ||= Admin.find(session[:admin_id]) if session[:admin_id]
+  end
+
   helper_method :current_user
+  helper_method :current_admin
 end
