a8932d5ff7d2d287e02a5675480e382724fdfeac
David Woolf <davidjohnwoolf@gmail.com>
2014-11-29 15:07:29 -0700

    change sessions routes to include dashes

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,11 @@ Rails.application.routes.draw do
   resources :users
   resources :admins
 
-  get '/login' => 'user_sessions#new', as: :log_in
-  post '/login' => 'user_sessions#create'
-  delete '/logout' => 'user_sessions#destroy', as: :log_out
+  get '/log-in' => 'user_sessions#new', as: :log_in
+  post '/log-in' => 'user_sessions#create'
+  delete '/log-out' => 'user_sessions#destroy', as: :log_out
 
-  get '/loginadmin' => 'admin_sessions#new', as: :log_in_admin
-  post '/loginadmin' => 'admin_sessions#create'
-  delete '/logoutadmin' => 'admin_sessions#destroy', as: :log_out_admin
+  get '/log-in-admin' => 'admin_sessions#new', as: :log_in_admin
+  post '/log-in-admin' => 'admin_sessions#create'
+  delete '/log-out-admin' => 'admin_sessions#destroy', as: :log_out_admin
 end
