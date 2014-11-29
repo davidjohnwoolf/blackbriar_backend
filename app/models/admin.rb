8da1b2da1ff8a89782db88d808e5a6c29f3a2bb8
David Woolf <davidjohnwoolf@gmail.com>
2014-11-28 23:30:57 -0700

    add validations and password methods to admin model

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,2 +1,23 @@
 class Admin < ActiveRecord::Base
+  attr_accessor :password
+  before_save :encrypt_password
+
+  validates :name, :presence => true, :uniqueness => true
+  validates :email, :presence => true, :uniqueness => true
+  validates :password, :presence => true, :confirmation => true
+  validates_length_of :password, :in => 6..20, :on => :create
+
+  def encrypt_password
+    self.password_salt = BCrypt::Engine.generate_salt
+    self.password_hash = BCrypt::Engine.hash_secret(password, password_salt)
+  end
+
+  def self.authenticate(email, password)
+    admin = Admin.where(email: email).first
+    if admin && admin.password_hash == BCrypt::Engine.hash_secret(password, admin.password_salt)
+      admin
+    else
+      nil
+    end
+  end
 end
