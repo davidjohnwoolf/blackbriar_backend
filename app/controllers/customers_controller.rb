792de377c147704e316dbeef3f4c441f711db82a
David Woolf <davidjohnwoolf@gmail.com>
2014-11-30 14:20:36 -0700

    create crud actions for customer in controller

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,2 +1,52 @@
 class CustomersController < ApplicationController
+  before_action :find_customer, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @customers = Customer.all
+  end
+
+  def show
+  end
+
+  def new
+    @customer = Customer.new
+  end
+
+  def create
+    @customer = Customer.new(customer_params)
+    if @customer.save
+      flash[:notice] = 'Customer created'
+      redirect_to customers_url
+    else
+      flash[:alert] = 'Error creating customer'
+      redirect_to new_customer_url
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @customer.update_attributes(customer_params)
+      redirect_to customer_url
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @customer.destroy
+    redirect_to customers_url
+  end
+
+  private
+
+  def find_customer
+    @customer = Customer.find(params[:id])
+  end
+
+  def customer_params
+    params.require(:customer).permit(:name, :install_date, :mmr, :sixty_month, :eft, :activation)
+  end
+
 end
