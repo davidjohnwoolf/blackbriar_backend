35ce1571cdaea5f035c53bded8709f60135e5fb4
David Woolf <davidjohnwoolf@gmail.com>
2014-11-30 16:23:45 -0700

    add validations to customer model

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,5 +1,10 @@
 class Customer < ActiveRecord::Base
   belongs_to :user
 
-  validates :user_id, presence: true
+  validates :name, presence: true
+  validates :install_date, presence: true
+  validates :mmr, presence: true
+  validates :sixty_month, presence: true
+  validates :eft, presence: true
+  validates :activation, presence: true
 end
