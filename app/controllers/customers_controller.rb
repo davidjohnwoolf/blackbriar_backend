a850681de5d5ba319402d9c47f65ae949e96df01
David Woolf <davidjohnwoolf@gmail.com>
2014-11-30 17:49:16 -0700

    add user_id to customer strong params and add user_id field to customer forms

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -14,7 +14,6 @@ class CustomersController < ApplicationController
 
   def create
     @customer = Customer.new(customer_params)
-    @customer.user_id = current_user.id
     if @customer.save
       flash[:notice] = 'Customer created'
       redirect_to customers_url
@@ -47,7 +46,7 @@ class CustomersController < ApplicationController
   end
 
   def customer_params
-    params.require(:customer).permit(:name, :install_date, :mmr, :sixty_month, :eft, :activation)
+    params.require(:customer).permit(:name, :install_date, :mmr, :sixty_month, :eft, :activation, :user_id)
   end
 
 end
