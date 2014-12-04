a282c35c6e1b18f42c15426308ee6db3529791da
David Woolf <davidjohnwoolf@gmail.com>
2014-12-03 19:01:26 -0700

    add stat attributes to users migration

--- db/migrate/20141123041643_create_users.rb
+++ db/migrate/20141123041643_create_users.rb
@@ -5,6 +5,9 @@ class CreateUsers < ActiveRecord::Migration
       t.string :email, null: false
       t.string :password_hash, null: false
       t.string :password_salt, null: false
+      t.decimal :eft_percent
+      t.decimal :sixty_month_percent
+      t.decimal :activation_percent
 
       t.timestamps
     end
