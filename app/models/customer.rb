09386b4f2dc254d676ba9fc3933779802cabac87
David Woolf <davidjohnwoolf@gmail.com>
2014-12-05 16:51:34 -0700

    add validations for new customer attributes

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -2,6 +2,20 @@ class Customer < ActiveRecord::Base
   belongs_to :user
 
   validates :name, presence: true
+  validates :address, presence: true
+  validates :city, presence: true
+  validates :state, presence: true
+  validates :zip, presence: true
+  validates :phone, presence: true, format: { with: /\A\d{3}-\d{3}-\d{4}\z/ }
+  validates :credit_grade, presence: true
+  validates :tech_name, presence: true
+  validates :package, presence: true
   validates :mmr, presence: true
+  validates :date_sold, presence: true
+  validates :date_installed, presence: true
+  validates :sixty_month, presence: true
+  validates :eft, presence: true
+  validates :activation, presence: true
+  validates :takeover, presence: true
   validates :user_id, presence: true
 end
