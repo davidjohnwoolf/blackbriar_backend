37ccf6987310b4a38a029be1867cdf216704a58f
David Woolf <davidjohnwoolf@gmail.com>
2014-12-05 14:52:54 -0700

    remove wrong validation in customer model

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -2,7 +2,6 @@ class Customer < ActiveRecord::Base
   belongs_to :user
 
   validates :name, presence: true
-  validates :install_date, presence: true
   validates :mmr, presence: true
   validates :user_id, presence: true
 end
